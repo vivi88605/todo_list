cb03cf9ac8780272cfa3c31375750d647c9fe754
vivi88605 <VIVI88605@gmail.com>
2023-05-17 17:37:33 +0800

    add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-//載入express
-const express = require('express')
+const express = require('express')//載入express
 const mongoose = require('mongoose')//載入mongoose
-//加入這段code 僅在非正式環境時使用dotenv
-if (process.env.NODE_ENV !== 'production') {
+const exphbs = require('express-handlebars')
+if (process.env.NODE_ENV !== 'production') {//加入這段code 僅在非正式環境時使用dotenv
   require('dotenv').config()
 }
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useNewUrlParser: true, useUnifiedTopology: true })//設定連線到MONGODB
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -22,7 +24,7 @@ db.once('open', () => {
 
 //首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 //設定port 3000
