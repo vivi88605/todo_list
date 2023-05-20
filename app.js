2b7d1651b51813808e74e025d6c8a916b2d92a8f
vivi88605 <VIVI88605@gmail.com>
2023-05-20 19:35:43 +0800

    add detail page

--- app.js
+++ app.js
@@ -42,6 +42,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 //設定port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
