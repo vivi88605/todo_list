72b634b9cbd4abd24793e8c2c860ddeeb365febc
vivi88605 <VIVI88605@gmail.com>
2023-08-13 16:27:31 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ db.once('open', () => {// 連線成功
 app.get('/', (req, res) => {
   todoData.find()//載入Todo Model裡的整包資料
     .lean()//把Mongoose裡的Model物件轉換成乾淨的Javascript資料陣列
+    .sort({ _id: 'asc' })//'asc'=ascending,升冪
     .then(todos => res.render('index', { todos }))//{ todos } = { todos:todos } //將todos傳給index樣板
     .catch(error => console.error(error))//錯誤處理
 })
